3c70287ca18f18307b27bc2d6d196c38d726798c
Wilson Duan <wduan@caltech.edu>
2024-04-23 02:02:29 -0700

    update script

--- script_cnn.sh
+++ script_cnn.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#SBATCH --job-name=Test
+#SBATCH --job-name=CNN
 #SBATCH --output=/central/groups/CS156b/2024/BroadBahnMi/%j.out
 #SBATCH --error=/central/groups/CS156b/2024/BroadBahnMi/%j.err
 #SBATCH -A CS156b
-#SBATCH --time=02:00:00
+#SBATCH --time=8:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=8G
-#SBATCH --gres gpu:1
+#SBATCH --mem=32G
+#SBATCH --gres gpu:2
 #SBATCH --mail-user=wduan@caltech.edu
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
