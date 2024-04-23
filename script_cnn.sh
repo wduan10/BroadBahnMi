b812d0d4b955d0600a1c77c3f74299439a1fbea6
Wilson Duan <wduan@caltech.edu>
2024-04-23 12:50:19 -0700

    test parameters

--- script_cnn.sh
+++ script_cnn.sh
@@ -4,11 +4,11 @@
 #SBATCH --output=/central/groups/CS156b/2024/BroadBahnMi/%j.out
 #SBATCH --error=/central/groups/CS156b/2024/BroadBahnMi/%j.err
 #SBATCH -A CS156b
-#SBATCH --time=8:00:00
+#SBATCH --time=1:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=32G
-#SBATCH --gres gpu:2
+#SBATCH --mem=16G
+#SBATCH --gres gpu:1
 #SBATCH --mail-user=wduan@caltech.edu
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
