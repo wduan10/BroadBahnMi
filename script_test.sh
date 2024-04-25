539432c17fef8f8c7103d0e6e0351d0e5ef6e44b
Wilson Duan <wduan@caltech.edu>
2024-04-24 23:54:37 -0700

    test

---
+++ script_test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=CNN
+#SBATCH --output=/central/groups/CS156b/2024/BroadBahnMi/%j.out
+#SBATCH --error=/central/groups/CS156b/2024/BroadBahnMi/%j.err
+#SBATCH -A CS156b
+#SBATCH --time=1:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=16G
+#SBATCH --gres gpu:1
+#SBATCH --mail-user=wduan@caltech.edu
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --partition=gpu
+
+source /home/wduan/venvs/CS156b/bin/activate
+
+cd /central/groups/CS156b/2024/BroadBahnMi/
+
+python main.py
