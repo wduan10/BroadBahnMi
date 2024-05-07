c27386accb444a82b7fdb73c69324b4aa914a367
oliiviaxu <olivia.xu@gmail.com>
2024-05-07 10:29:31 -0700

    resnet

--- pathology_script.py
+++ pathology_script.py
@@ -7,8 +7,9 @@ from matplotlib import pyplot as plt
 import tensorflow as tf
 import keras 
 from keras.src.legacy.preprocessing.image import ImageDataGenerator
-from keras import Model
+from keras import Model, Sequential
 from keras import layers 
+from keras.src.layers import Conv2D, MaxPooling2D, Dense, Flatten
 from sklearn.model_selection import train_test_split
 from keras.src.applications.densenet import DenseNet121, preprocess_input 
 from keras.src.applications.resnet import ResNet50, preprocess_input
@@ -107,6 +108,7 @@ def get_pathology(pathology):
 # Handling test data 
 test_df = pd.DataFrame()
 test_df['filename'] = df_test['Path']
+display(test_df.head())
 
 test_datagen = ImageDataGenerator(rescale=1./255)
 
@@ -125,15 +127,43 @@ classes = ["No Finding", "Enlarged Cardiomediastinum", "Cardiomegaly", "Lung Opa
 
 pathology = "Fracture"
 train_data, val_data = get_pathology(pathology)
+display(train_data.head())
+display(val_data.head())
+
+class_mapping = train_data.class_indices 
+print(class_mapping)
 
 # VGG16 Model
 # conv_base = VGG16(include_top=False, weights='imagenet', input_shape=(224, 224, 3))
 
 # Resnet 
-# conv_base = ResNet50(include_top=False, weights='imagenet', input_shape=(224, 224, 3))
+conv_base = ResNet50(include_top=False, weights='imagenet', input_shape=(224, 224, 3))
 
 # DenseNet
-conv_base = DenseNet121(include_top=False, weights='imagenet', input_shape=(224, 224, 3))
+# conv_base = DenseNet121(include_top=False, weights='imagenet', input_shape=(224, 224, 3))
+
+# AlexNet
+# model = Sequential()
+
+# # Layer 1: Convolutional layer with 64 filters of size 11x11x3
+# model.add(Conv2D(filters=64, kernel_size=(11,11), strides=(4,4), padding='valid', activation='relu', input_shape=(224,224,3)))
+
+# # Layer 2: Max pooling layer with pool size of 3x3
+# model.add(MaxPooling2D(pool_size=(3,3), strides=(2,2)))
+
+# # Layer 3-5: 3 more convolutional layers with similar structure as Layer 1
+# model.add(Conv2D(filters=192, kernel_size=(5,5), padding='same', activation='relu'))
+# model.add(MaxPooling2D(pool_size=(3,3), strides=(2,2)))
+# model.add(Conv2D(filters=384, kernel_size=(3,3), padding='same', activation='relu'))
+# model.add(Conv2D(filters=256, kernel_size=(3,3), padding='same', activation='relu'))
+# model.add(MaxPooling2D(pool_size=(3,3), strides=(2,2)))
+
+# # Layer 6: Fully connected layer with 4096 neurons
+# model.add(Flatten())
+# model.add(Dense(4096, activation='relu'))
+
+# # Layer 7: Fully connected layer with 4096 neurons
+# model.add(Dense(4096, activation='relu'))
 
 # Customize top layer
 top_layer = conv_base.output
@@ -146,13 +176,6 @@ output_layer = keras.layers.Dense(3, activation='softmax')(top_layer) # Predicti
 model = Model(inputs=conv_base.input, outputs=output_layer)
 
 optimizer = keras.optimizers.Adam(learning_rate=LEARNING_RATE)
-
-# For lower num_epochs 
-# lr_schedule = keras.optimizers.schedules.ExponentialDecay(
-#     initial_learning_rate=LEARNING_RATE,
-#     decay_steps=10000,
-#     decay_rate=0.9)
-# optimizer = keras.optimizers.SGD(learning_rate=lr_schedule)
 
 model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
 
@@ -167,10 +190,9 @@ validation_loss = history.history['val_loss']
 for i in range(NUM_EPOCHS):
     print(f"Epoch {i+1}: Training Loss = {training_loss[i]:.4f}, Validation Loss = {validation_loss[i]:.4f}")
 
-# Dataframe of predictions for 3 classes 
+# Dataframe of predictions 
 predictions = model.predict(test_data)
-columns = list(val_data.class_indices.keys()) 
-preds = pd.DataFrame(predictions, columns=columns)
+preds = pd.DataFrame(predictions) 
 
 output_dir = 'predictions'   
 now = datetime.datetime.now()
