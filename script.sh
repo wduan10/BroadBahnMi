125b87700e6afc3c26a97cddc788fe83d9ca37fb
oliiviaxu <olivia.xu@gmail.com>
2024-05-16 00:36:43 -0700

    fracture

--- script.sh
+++ script.sh
@@ -4,11 +4,11 @@
 #SBATCH --output=/home/oxu/BroadBahnMi/out/%j.out
 #SBATCH --error=/home/oxu/BroadBahnMi/out/%j.err
 #SBATCH -A CS156b
-#SBATCH --time=72:00:00
+#SBATCH --time=8:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=32G
-#SBATCH --gres gpu:4
+#SBATCH --mem=8G
+#SBATCH --gres gpu:2
 #SBATCH --mail-user=oxu@caltech.edu
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
