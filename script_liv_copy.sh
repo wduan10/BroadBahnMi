f78c0198f2a3619e9165d8861a0ab7f626084cdf
Wilson Duan <wduan@caltech.edu>
2024-05-24 09:40:09 -0700

    liv copy

---
+++ script_liv_copy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --job-name=LivsModel
+#SBATCH --output=/home/oxu/BroadBahnMi/out/%j.out
+#SBATCH --error=/home/oxu/BroadBahnMi/out/%j.err
+#SBATCH -A CS156b
+#SBATCH --time=12:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=32G
+#SBATCH --gres gpu:4
+#SBATCH --mail-user=oxu@caltech.edu
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --partition=gpu
+
+source /home/oxu/my_venv/bin/activate
+
+# cd /home/oxu/BroadBahnMi
+cd /central/groups/CS156b/2024/BroadBahnMi/
+
+python CNN_single_copy.py
