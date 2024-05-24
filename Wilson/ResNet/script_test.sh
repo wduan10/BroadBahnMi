2cc59169b561592831699c309119e8ee51ca9e4c
Wilson Duan <wduan@caltech.edu>
2024-05-24 01:04:32 -0700

    test

---
+++ Wilson/ResNet/script_test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=ResNet
+#SBATCH --output=/central/groups/CS156b/2024/BroadBahnMi/%j.out
+#SBATCH --error=/central/groups/CS156b/2024/BroadBahnMi/%j.err
+#SBATCH -A CS156b
+#SBATCH --time=2:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=32G
+#SBATCH --gres gpu:4
+#SBATCH --mail-user=wduan@caltech.edu
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --partition=gpu
+
+source /home/wduan/venvs/CS156b/bin/activate
+
+cd /central/groups/CS156b/2024/BroadBahnMi/Wilson/ResNet
+
+python ResNet_default.py hpc
