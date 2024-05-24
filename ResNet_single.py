7362dbef3e8a1f2eeaf125b65871763c68fd6f36
Wilson Duan <wduan@caltech.edu>
2024-05-23 22:33:44 -0700

    change transform

--- ResNet_single.py
+++ ResNet_single.py
@@ -138,22 +138,22 @@ class TestImageDataset(Dataset):
 
 
 # default transform:
-transform = transforms.Compose([
-    transforms.Lambda(lambda image: image.convert('RGB')),
-    transforms.Resize((256, 256)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-])
-
-# transform with random flipping and cropping:
 # transform = transforms.Compose([
 #     transforms.Lambda(lambda image: image.convert('RGB')),
-#     transforms.Resize((300, 300)),
+#     transforms.Resize((256, 256)),
 #     transforms.ToTensor(),
 #     transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-#     transforms.RandomHorizontalFlip(),
-#     transforms.RandomCrop((256, 256))
 # ])
+
+# transform with random flipping and cropping:
+transform = transforms.Compose([
+    transforms.Lambda(lambda image: image.convert('RGB')),
+    transforms.Resize((300, 300)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomCrop((256, 256))
+])
 
 # transform with Gaussian blur:
 # transform = transforms.Compose([
@@ -180,7 +180,6 @@ test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
 
 
 model = ResNet50(3)
-model = nn.DataParallel(model)
 model = model.to(device)
 
 criterion = nn.CrossEntropyLoss()
