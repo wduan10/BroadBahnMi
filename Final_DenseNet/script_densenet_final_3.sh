0d08da2c4ad13de6794863a8bff48cf719d3bb19
Wilson Duan <wduan@caltech.edu>
2024-05-26 13:46:12 -0700

    changes

---
+++ Final_DenseNet/script_densenet_final_3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=DenseNet
+#SBATCH --output=/central/groups/CS156b/2024/BroadBahnMi/%j.out
+#SBATCH --error=/central/groups/CS156b/2024/BroadBahnMi/%j.err
+#SBATCH -A CS156b
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32G
+#SBATCH --gres gpu:4
+#SBATCH --mail-user=wduan@caltech.edu
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --partition=gpu
+
+source /home/wduan/venvs/CS156b/bin/activate
+
+cd /central/groups/CS156b/2024/BroadBahnMi/Final_DenseNet
+
+python DenseNet_final.py hpc 3
