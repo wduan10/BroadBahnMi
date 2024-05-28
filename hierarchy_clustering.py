bf40fd46f88c4b3491207c8f511449c53991b5c3
GMalueg <106219954+GMalueg@users.noreply.github.com>
2024-05-27 19:08:05 -0700

    Create hierarchy_clustering.py

---
+++ hierarchy_clustering.py
@@ -0,0 +1,137 @@
+import os
+import sys
+import torch
+import torch.nn as nn
+import pandas as pd
+import torchvision
+from torchvision import transforms
+from torch.utils.data import DataLoader, Dataset
+from PIL import Image
+from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
+import numpy as np
+from sklearn.neighbors import NearestNeighbors
+import matplotlib.pyplot as plt
+from torchvision.models import resnet18, ResNet18_Weights, resnet50, ResNet50_Weights
+print('Importing libraries... Done.')
+
+# Setup check
+hpc = False
+if len(sys.argv) > 1 and sys.argv[1] == 'hpc':
+    hpc = True
+
+# Data paths
+labels_path_train = '/groups/CS156b/data/student_labels/train2023.csv' if hpc else 'data/train/labels/labels.csv'
+labels_path_test = '/groups/CS156b/data/student_labels/test_ids.csv' if hpc else 'data/test/ids.csv'
+img_dir = '/groups/CS156b/data' if hpc else 'data'
+
+device = torch.device('cuda' if (torch.cuda.is_available()) else 'cpu')
+print(hpc, device)
+# Load data
+df_train = pd.read_csv(labels_path_train)
+df_test = pd.read_csv(labels_path_test)
+
+# Data preprocessing
+def parse_labels(df):
+    df.fillna(0, inplace=True)
+    return df  # Adjust this slice according to your actual data structure
+
+class TrainImageDataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform):
+        if hpc:
+            self.image_labels = parse_labels(pd.read_csv(annotations_file)[:-1])
+        else:
+            self.img_labels = parse_labels(pd.read_csv(annotations_file))
+        self.img_dir = img_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        row = self.img_labels.iloc[idx]
+        image_path = os.path.join(self.img_dir, row['Path'])
+        image = Image.open(image_path).convert('RGB')
+        image = self.transform(image)
+        labels = torch.tensor(row[-9:].values.astype(np.float32))
+        return image, labels
+
+class TestImageDataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform):
+        self.img_labels = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        row = self.img_labels.iloc[idx]
+        image_path = os.path.join(self.img_dir, row['Path'])
+        image = Image.open(image_path).convert('RGB')
+        image = self.transform(image)
+        return image, row['Id']
+
+transform = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+])
+
+resnet = torchvision.models.resnet50(weights=ResNet50_Weights.DEFAULT)
+resnet.fc = nn.Identity()
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+resnet = resnet.to(device)
+resnet.eval()
+
+def extract_features_and_pathologies(loader):
+    features = []
+    pathologies = []
+    with torch.no_grad():
+        for images, labels in loader:
+            images = images.to(device)  
+            feats = resnet(images) 
+            features.append(feats.cpu().numpy())
+            pathologies.append(labels.cpu().numpy())  
+    return np.vstack(features), np.vstack(pathologies)
+    
+train_dataset = TrainImageDataset(labels_path_train, img_dir, transform)
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+train_features, train_pathologies = extract_features_and_pathologies(train_loader)
+
+linkage_matrix = linkage(train_features, method='ward')
+plt.figure(figsize=(10, 7))
+dendrogram(linkage_matrix)
+plt.title('Hierarchical Clustering Dendrogram')
+plt.xlabel('Sample index')
+plt.ylabel('Distance')
+plt.show()
+
+cluster_labels = fcluster(linkage_matrix, t=5, criterion='distance')
+cluster_pathologies = {}
+for idx, cluster in enumerate(cluster_labels):
+    if cluster not in cluster_pathologies:
+        cluster_pathologies[cluster] = []
+    cluster_pathologies[cluster].append(train_pathologies[idx])
+cluster_means = {cluster: np.mean(vals, axis=0) for cluster, vals in cluster_pathologies.items()}
+
+test_dataset = TestImageDataset(labels_path_test, img_dir, transform)
+test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+test_features, test_ids = extract_features_and_pathologies(test_loader)
+
+nn_model = NearestNeighbors(n_neighbors=1)
+nn_model.fit(train_features)
+_, indices = nn_model.kneighbors(test_features)
+predicted_pathologies = [cluster_means[cluster_labels[idx]] for idx in indices.flatten()]
+
+df_results = pd.DataFrame(predicted_pathologies, columns=df_train.columns[-9:])
+test_ids = test_ids.flatten()
+df_results.insert(0, 'Id', test_ids)
+
+if (hpc):
+    output_dir = '/groups/CS156b/2024/BroadBahnMi/predictions'
+else:
+    output_dir = 'predictions'
+
+filename = 'pathologies_cluster'
+full_path = os.path.join(output_dir, f'preds_{filename}.csv')
+df_results.to_csv(full_path, index=False)
