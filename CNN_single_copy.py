b4358b3f0b5a633b16d74e2b0f09d7430a7ddb6f
oliiviaxu <olivia.xu@gmail.com>
2024-05-30 10:07:28 -0700

    frac

--- CNN_single_copy.py
+++ CNN_single_copy.py
@@ -26,10 +26,10 @@ hpc = False
 if len(sys.argv) > 1 and sys.argv[1] == 'hpc':
     hpc = True 
 
-pathology = 'Enlarged Cardiomediastinum'
+pathology = 'Fracture'
 
 lr = 0.0002
-n_epochs = 15
+n_epochs = 1
 batch_size = 128
 n_cpu = 4 if hpc else 0
 device = torch.device('cuda:0' if (torch.cuda.is_available()) else 'cpu')
