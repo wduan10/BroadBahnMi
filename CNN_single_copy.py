0c018b83b253c890bcf553d158f02c43f896f673
oliiviaxu <olivia.xu@gmail.com>
2024-05-31 23:24:03 -0700

    lung

--- CNN_single_copy.py
+++ CNN_single_copy.py
@@ -26,7 +26,7 @@ hpc = False
 if len(sys.argv) > 1 and sys.argv[1] == 'hpc':
     hpc = True 
 
-pathology = "Cardiomegaly"
+pathology = "Lung Opacity"
 
 classes = ["No Finding", "Enlarged Cardiomediastinum", "Cardiomegaly", "Lung Opacity",
            "Pneumonia", "Pleural Effusion", "Pleural Other", "Fracture", "Support Devices"]
